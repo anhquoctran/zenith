23f4a628a505052f521ef72288ebc57e5d114d04
anhquoctran <aquoc.hue@outlook.com>
2026-06-30 11:34:59 +0700

    chore: ignore build artifacts and IDE metadata files

--- Zenith.UI/obj/Debug/net10.0/win-x64/Zenith.UI.AssemblyInfo.cs
+++ Zenith.UI/obj/Debug/net10.0/win-x64/Zenith.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zenith")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a8939cf8237141c34976b202481d2df815af784")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zenith.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zenith.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
